77d29b8094a9e0f8bd30cc59276a1b3add5aea7d
Hitesh Kumar <kmhitesh@amazon.com>
2023-11-26 12:49:30 +0530

    Fixed Scrapper not working

--- scraper.go
+++ scraper.go
@@ -19,37 +19,17 @@ func checkErrors(err error) {
 	}
 }
 
-//func generatePostRequestData() url.Values {
-//	loginRequest := url.Values{}
-//	loginRequest.Set("__EVENTTARGET", "")
-//	loginRequest.Set("__EVENTARGUMENT", "")
-//	loginRequest.Set("__VIEWSTATE", "/wEPDwULLTE1NjMxNjMxNzFkZCo3T3kAnddTDFryr26qaofiTp5p")
-//	loginRequest.Set("__VIEWSTATEGENERATOR", "C2EE9ABB")
-//	loginRequest.Set("__EVENTVALIDATION", "/wEdAAYNcEy/uvEwBm4by+oKLWkjSvD5Cbpu3w0ab2H9f5rbFEPTPkdPWl+8YN2NtDCtxifN+DvxnwFeFeJ9MIBWR693w+qCzNvQHKCQwl8+YzOKE62xJNKuHibH70Ul6qoa4F8sDaR1uxEyo1xbP9xcXI4vvNcYtQ==")
-//	loginRequest.Set("TxtEmail", "id")
-//	loginRequest.Set("txtPass", "pass")
-//	loginRequest.Set("Button1", "Login")
-//	loginRequest.Set("txtEmailId", "")
-//	return loginRequest
-//}
-
 func setHeaders(req *http.Request, length int64, cookies string) {
 	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Set("Accept-Encoding", "deflate")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,gu;q=0.8,hi;q=0.7")
-	//req.Header.Set("Cache-Control", "max-age=0")
-	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
 	req.Header.Set("Content-Length", strconv.FormatInt(length, 10))
-	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.Header.Set("Cookie", cookies)
-	req.Header.Set("DNT", "1")
-	req.Header.Set("Host", "psd.bits-pilani.ac.in")
 	req.Header.Set("Origin", "http://psd.bits-pilani.ac.in")
-	req.Header.Set("Referer", "ttp://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Mobile Safari/537.36)")
+	req.Header.Set("Referer", "http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0")
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-
 }
 
 func decodeJSON(response *io.ReadCloser) []map[string]interface{} {
@@ -72,6 +52,26 @@ func findInMapArray(mapArray []map[string]interface{}, key string, value interfa
 		}
 	}
 	return nil
+}
+
+func postRequest(url string, data string, cookies string) []map[string]interface{} {
+	req, err := http.NewRequest("POST", url, strings.NewReader(data))
+	checkErrors(err)
+
+	setHeaders(req, int64(len(data)), cookies)
+
+	client := &http.Client{}
+	checkErrors(err)
+	resp, err := client.Do(req)
+	checkErrors(err)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("HTTP Response code: %d\n%s", resp.StatusCode, body)
+	}
+
+	return decodeJSON(&resp.Body)
 }
 
 func writeCSV(stationList, preferenceList, problemBank []map[string]interface{}) {
@@ -143,26 +143,6 @@ func writeCSV(stationList, preferenceList, problemBank []map[string]interface{})
 	checkErrors(err)
 }
 
-func postRequest(url string, data string, cookies string) []map[string]interface{} {
-	req, err := http.NewRequest("POST", url, strings.NewReader(data))
-	checkErrors(err)
-	//dataContent, err := ioutil.ReadAll(data)
-	checkErrors(err)
-	setHeaders(req, int64(len(data)), cookies)
-
-	client := &http.Client{}
-	checkErrors(err)
-	resp, err := client.Do(req)
-	checkErrors(err)
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalln("HTTP Response code: " + strconv.FormatInt(int64(resp.StatusCode), 10))
-	} else {
-		fmt.Println(url + " " + strconv.FormatInt(int64(resp.StatusCode), 10))
-	}
-	return decodeJSON(&resp.Body)
-}
-
 func getUpdateJSON() []byte {
 	csvFile, err := os.Open("StationDetails.csv")
 	checkErrors(err)
@@ -199,69 +179,29 @@ func getUpdateJSON() []byte {
 	data := "{jsondata: \"" + url.QueryEscape(jsondata) + "\", jsonvalue: \"\" , contistation: \"0\"}"
 
 	return []byte(data)
-
-	// Good method which doesn't work as the guy doesn't use JSON but a custom shitty version of it
-	// for some godforsaken reason -_-
-	//
-	//var finalPostData = make(map[string]interface{}, 3)
-	//finalPostData["jsonvalue"] = ""
-	//finalPostData["contistation"] = "0"
-	//
-	//var preferenceListArr []map[string]string
-	//
-	//record, err := csvReader.Read() //First Row has titles
-	//
-	////Make JSONArray for sending
-	//var i int64 = 1
-	//for {
-	//	var StationEntry = make(map[string]string, 4)
-	//	record, err = csvReader.Read()
-	//	if err != io.EOF {
-	//		checkErrors(err)
-	//	} else {
-	//		break
-	//	}
-	//	StationEntry["isActive"] = "1"
-	//	StationEntry["PreferenceNo"] = strconv.FormatInt(i, 10)
-	//	i += 1
-	//	StationEntry["StationId"] = record[0]
-	//	var accommodation string
-	//	if string(record[7][0]) == "Y" || string(record[7][0]) == "y" {
-	//		accommodation = "true"
-	//	} else {
-	//		accommodation = "false"
-	//	}
-	//	StationEntry["Accomodation"] = accommodation
-	//
-	//	preferenceListArr = append(preferenceListArr, StationEntry)
-	//
-	//}
-	//
-	//updateList, err := json.Marshal(preferenceListArr)
-	//checkErrors(err)
-	//finalPostData["jsondata"] = url.PathEscape(strings.ReplaceAll(string(updateList), "\"", "'"))
-	//fmt.Println(finalPostData)
-	//updateJSON, err := json.Marshal(finalPostData)
-	//return updateJSON
-	//fmt.Println(preferenceListArr)
 }
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run yourscript.go -g <cookies> OR go run yourscript.go -u <cookies>")
+		return
+	}
+
 	if os.Args[1] == "-g" {
-		//Create CSV
+		// Your existing logic for fetching data
 		stationList := postRequest("http://psd.bits-pilani.ac.in/Student/StudentStationPreference.aspx/getinfoStation", "{CompanyId: \"0\" }", os.Args[2])
 		problemBank := postRequest("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx/getPBdetail", "{batchid: \"undefined\" }", os.Args[2])
 		preferenceList := postRequest("http://psd.bits-pilani.ac.in/Student/StudentStationPreference.aspx/chkStationpref", "{contactid: \"0\"}", os.Args[2])
 		writeCSV(stationList, preferenceList, problemBank)
 	}
+
 	if os.Args[1] == "-u" {
-		//Update pref list on website
+		// Your existing logic for updating data
 		updateJSON := getUpdateJSON()
-		//fmt.Println(string(updateJSON))
 		postRequest("http://psd.bits-pilani.ac.in/Student/StudentStationPreference.aspx/saveStudentStationPref", string(updateJSON), os.Args[2])
 	}
+
 	if os.Args[1] != "-u" && os.Args[1] != "-g" {
 		fmt.Println("Wrong Argument: " + os.Args[1])
 	}
-
 }
